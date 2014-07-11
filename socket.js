3e2eb908e40c1e15306e9a28e4d36dae793e3fa8
jbouzekri <jonathan.bouzekri@gmail.com>
2014-07-11 23:18:27 +0200

    rename tools module

--- socket.js
+++ socket.js
@@ -1,5 +1,5 @@
 var cookieParser   = require('cookie-parser')();
-var manager        = require('./manager.js');
+var tools          = require('./tools.js');
 var SessionSockets = require('session.socket.io-express4');
 
 module.exports.listen = function(app, sessionStore, port, callback) {
@@ -22,25 +22,25 @@ module.exports.listen = function(app, sessionStore, port, callback) {
         }
 
         // Operators join all the same room to receive all message
-        if (manager.isOperator(session)) {
+        if (tools.isOperator(session)) {
             console.log('[socket #'+socket.id+'] Operator detected');
             socket.join('managers');
-            manager.registerOperatorSocket(socket, session);
+            tools.registerOperatorSocket(socket, session);
         } else {
             console.log('[socket #'+socket.id+'] User detected');
               // Register user socket for future reference
-            manager.registerUserSocket(socket, session);
+            tools.registerUserSocket(socket, session);
         }
 
         socket.on('chat-state', function(msg) {
             console.log('[socket #'+socket.id+'] chat-state message received');
-            socket.emit('chat-state', manager.buildStateMessage());
+            socket.emit('chat-state', tools.buildStateMessage());
             console.log('[socket #'+socket.id+'] chat-state message sent');
         });
 
         socket.on('online-toggle', function(msg){
             console.log('[socket #'+socket.id+'] online-toggle message received');
-            if (manager.isOperator(session)) {
+            if (tools.isOperator(session)) {
                 var online = app.get('online_state').toggleState().online;
             }
             socket.emit('online-state', app.get('online_state'));
@@ -48,23 +48,23 @@ module.exports.listen = function(app, sessionStore, port, callback) {
         });
 
         socket.on('user-message', function(msg) {
-            if (manager.validateMessage(msg)) {
-              io.to('managers').emit('user-message', manager.buildUserMessage(session, msg));
+            if (tools.validateMessage(msg)) {
+              io.to('managers').emit('user-message', tools.buildUserMessage(session, msg));
             }
         });
 
         socket.on('operator-message', function(msg) {
-            var answerSocket = manager.findUserSockets(msg.customerId);
+            var answerSocket = tools.findUserSockets(msg.customerId);
             for (id in answerSocket) {
-                answerSocket[id].emit('operator-message', manager.buildOperatorMessage(session, msg));
+                answerSocket[id].emit('operator-message', tools.buildOperatorMessage(session, msg));
             }
             io.to('managers').emit('operator-message', msg);
         });
 
         socket.on('disconnect', function(){
             console.log('[socket #'+socket.id+'] disconnected');
-            manager.deleteSocket(socket, session);
-            io.to('managers').emit('chat-state', manager.buildStateMessage());
+            tools.deleteSocket(socket, session);
+            io.to('managers').emit('chat-state', tools.buildStateMessage());
         });
     });
 
